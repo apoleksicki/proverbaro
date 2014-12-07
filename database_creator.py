203dcc6ff6b578592a6267ed2af4eae84e71cb60
Antoni Piotr Oleksicki <ap.oleksicki@gmail.com>
2014-12-07 19:38:16 +0100

    Added module for creation of the database

---
+++ database_creator.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+import sqlite3
+
+def create_database():
+    conn = sqlite3.connect('proverbaro.db')
+    print 'Proverbaro database created'
+    with conn:
+        conn.execute('DROP TABLE Proverbs')
+        conn.execute('CREATE TABLE Proverbs(id INTEGER PRIMARY KEY, text TEXT, shown_times INTEGER DEFAULT 0)')
+        with open('output.txt', 'r') as proverbs_file:
+            for proverb in proverbs_file:
+                conn.execute('INSERT INTO Proverbs(text) VALUES (\'%s\');' % proverb.replace('\'', '\'\''))
+
+
+create_database()
+
+
+
+
