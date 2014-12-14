7ca9976af54cbf1712f67cd50c6e66daf28196c4
Antoni Piotr Oleksicki <ap.oleksicki@gmail.com>
2014-12-14 15:53:53 +0100

    Code cleanup
    Removed unused functions.

--- proverbaro.py
+++ proverbaro.py
@@ -6,6 +6,7 @@ from sqlalchemy import create_engine
 from sqlalchemy import func
 from sqlite3 import dbapi2 as sqlite
 from random import shuffle
+import sys
 
 e = create_engine('sqlite+pysqlite:///proverbaro.db', module=sqlite, encoding="utf-8")
 Base = declarative_base()
@@ -16,53 +17,21 @@ class Proverb(Base):
     text = Column(Unicode, nullable=False)
     shown_times = Column(Integer, default=0, nullable=False)
 
-def get_proverbs(session, debug=False):
-    proverbs_query = session.query(Proverb).filter(Proverb.shown_times == s.query(func.min(Proverb.shown_times)))
-    if debug:
-        proverbs_query = proverbs_query.limit(10)
-    proverbs = proverbs_query.all()
-    shuffle(proverbs)
-    return proverbs
+def fetch_next_proverb(session):
+    return session.query(Proverb).filter(Proverb.shown_times == s.query(func.min(Proverb.shown_times))).order_by(func.random()).first()
 
-session = Session(bind=e)
-proverbs_query = session.query(Proverb).filter(Proverb.shown_times == s.query(func.min(Proverb.shown_times))).order_by(func.random())
-proverbs_query
-proverb = proverbs_query.first()
-proverb.text
-
-def mark_as_shown(session, proverb):
-    proverb.id += 1
-    session.add(proverb)
-
-def show_proverbs(proverbs):
-    for proverb in proverbs:
+def show_proverb():
         session = Session(bind=e)
         try:
-            mark_as_shown(session, proverb)
+            proverb = fetch_next_proverb(session)
+            proverb.shown_times += 1
             session.commit()
+            print proverb.text.encode("utf-8")
         except:
+            print sys.exc_info()
             session.rollback()
         finally:
             session.close()
-        print proverb.text.encode("utf-8")
-
-def main_loop():
-    session = Session(bind=e)
-    proberbs = None
-    try:
-        proverbs = get_proverbs(session, True)
-    finally:
-        session.close()
-    show_proverbs(proverbs)
-
-main_loop()
-
-def print_all_proverbs():
-    for p in get_proverbs(Session(bind=e), debug=True):
-        print p.shown_times, p.text.encode("utf-8")
-
-print_all_proverbs()
 
 if __name__ == '__main__':
-    for p in s.query(Proverb).all():
-        print p.shown_times, p.text.encode("utf-8")
+    show_proverb()
