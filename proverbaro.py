460a415c419375a908e2d3aabc3b99a9ce66f094
Antoni Piotr Oleksicki <ap.oleksicki@gmail.com>
2014-12-14 13:00:37 +0100

    Added a proverbs fetching function

--- proverbaro.py
+++ proverbaro.py
@@ -3,7 +3,9 @@ from sqlalchemy import Column, Integer, Unicode
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
+from sqlalchemy import func
 from sqlite3 import dbapi2 as sqlite
+from random import shuffle
 
 e = create_engine('sqlite+pysqlite:///proverbaro.db', module=sqlite, encoding="utf-8")
 Base = declarative_base()
@@ -16,15 +18,24 @@ class Proverb(Base):
 
 s = Session(bind=e)
 
+def find_min_shown_times():
+    return s.query(func.min(Proverb.shown_times))
+
+def get_proverbs():
+    s = Session(bind=e)
+    proverbs = s.query(Proverb).filter(Proverb.shown_times == s.query(func.min(Proverb.shown_times))).all()
+    shuffle(proverbs)
+    return proverbs
+
+proverbs = get_proverbs()
+len(proverbs)
 
 def print_all_proverbs():
     s = Session(bind=e)
     for p in s.query(Proverb).all():
         print p.shown_times, p.text.encode("utf-8")
 
-
 print_all_proverbs()
-
 
 if __name__ == '__main__':
     for p in s.query(Proverb).all():
