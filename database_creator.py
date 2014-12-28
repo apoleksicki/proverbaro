1847644900df10b79e50e91aa5c8215364effe0d
Antoni Piotr Oleksicki <ap.oleksicki@gmail.com>
2014-12-28 22:11:27 +0000

    Fixed a bug in database_creator.py

--- database_creator.py
+++ database_creator.py
@@ -5,7 +5,7 @@ def create_database():
     conn = sqlite3.connect('proverbaro.db')
     print 'Proverbaro database created'
     with conn:
-        conn.execute('DROP TABLE Proverbs')
+        conn.execute('DROP TABLE if exists Proverbs')
         conn.execute('CREATE TABLE Proverbs(id INTEGER PRIMARY KEY, text TEXT, shown_times INTEGER DEFAULT 0)')
         with open('output.txt', 'r') as proverbs_file:
             for proverb in proverbs_file:
