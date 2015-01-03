276d93efaa9128a25dbdb12e90ab7b72f0def820
Antoni Piotr Oleksicki <ap.oleksicki@gmail.com>
2015-01-03 21:36:09 +0100

    Created basic routing for presenting proverbs

--- proverbaro_web.py
+++ proverbaro_web.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
-from sqlalchemy import desc
+from sqlalchemy import desc, and_
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///proverbaro.db'
@@ -14,11 +14,26 @@ class Proverb(db.Model):
     shown_times = db.Column(db.Integer, default=0, nullable=False)
     shown_last_time = db.Column(db.DateTime, default=None)
 
+class PostId(db.Model):
+    __tablename__ = 'Post_Ids'
 
-@app.route('/')
-def hello_world():
-    return Proverb.query.order_by(desc(Proverb.shown_last_time)).first().text
+    def __init__(self, publish_date, publish_id, proverb_id):
+        self.publish_date = publish_date
+        self.publish_id = publish_id
+        self.proverb_id = proverb_id
 
+    id = db.Column(db.Integer, primary_key=True)
+    publish_date = db.Column(db.Date, nullable=False)
+    publish_id = db.Column(db.Integer, nullable=False)
+    proverb_id = db.Column(db.Integer, db.ForeignKey('Proverbs.id'),
+                           nullable=False)
+
+
+@app.route('/<date>/<int:publish_id>')
+def show_proverb(date, publish_id):
+    return Proverb.query.join(PostId).filter(and_(
+            PostId.publish_date == date,
+            PostId.publish_id == publish_id)).first().text
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
