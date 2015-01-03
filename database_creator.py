3afc868d50ed273e14eafb35866f5327d3851ca2
Antoni Piotr Oleksicki <ap.oleksicki@gmail.com>
2015-01-03 13:30:58 +0100

    PEP 8 guidelines applied to database_creator.py

--- database_creator.py
+++ database_creator.py
@@ -1,19 +1,19 @@
 # -*- coding: utf-8 -*-
 import sqlite3
 
+
 def create_database():
     conn = sqlite3.connect('proverbaro.db')
     print 'Proverbaro database created'
     with conn:
         conn.execute('DROP TABLE if exists Proverbs')
-        conn.execute('CREATE TABLE Proverbs(id INTEGER PRIMARY KEY, text TEXT, shown_times INTEGER DEFAULT 0, shown_last_time TEXT DEFAULT null)')
+        conn.execute('CREATE TABLE Proverbs(id INTEGER PRIMARY KEY, text TEXT,'
+                     'shown_times INTEGER DEFAULT 0,'
+                     'shown_last_time TEXT DEFAULT null)')
         with open('output.txt', 'r') as proverbs_file:
             for proverb in proverbs_file:
-                conn.execute('INSERT INTO Proverbs(text) VALUES (\'%s\');' % proverb.replace('\'', '\'\''))
+                conn.execute('INSERT INTO Proverbs(text) VALUES (\'%s\');'
+                             % proverb.replace('\'', '\'\''))
 
 if __name__ == '__main__':
     create_database()
-
-
-
-
