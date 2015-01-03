e2bc36eeb7514860457e3e386702335093a2e6cd
Antoni Piotr Oleksicki <ap.oleksicki@gmail.com>
2015-01-03 14:35:46 +0100

    PEP 8 guidelines applied to wiki wikipage_parser.py

--- wikipage_parser.py
+++ wikipage_parser.py
@@ -2,12 +2,14 @@
 import urllib
 from HTMLParser import HTMLParser
 
+
 def fetch_wikpage():
     page = urllib.urlopen("http://eo.wikiquote.org/wiki/Zamenhofa_proverbaro")
 
     file = open('website.html', 'w')
     file.write(page.read())
     file.close()
+
 
 def parse_wikipage():
     page = None
@@ -19,6 +21,7 @@ def parse_wikipage():
         for proverb in parser.parsed:
             output_file.write(proverb + '\n')
 
+
 class ProverbsWikipageParser(HTMLParser):
     def __init__(self):
         HTMLParser.__init__(self)
@@ -29,30 +32,37 @@ class ProverbsWikipageParser(HTMLParser):
 
     def _change_state(self, tag):
         if tag == 'noscript':
-        #Encountering means the content part has ended and now some general page stuff will appear
+            # Encountering means the content part has ended and now some
+            # general page stuff will appear
             self._parsing_finished = True
         if tag == 'li' and not self._parsing_finished:
             self._parsing_proverb = not self._parsing_proverb
             self._proverb = ''
+
     def handle_starttag(self, tag, attrs):
         self._change_state(tag)
+
     def handle_endtag(self, tag):
         if self._parsing_proverb and tag == 'li':
             self._parsed.append(self._proverb)
             print(self._proverb)
         self._change_state(tag)
+
     def handle_data(self, data):
         if self._parsing_proverb:
             self._proverb += data
+
     @property
     def parsed(self):
         return self._parsed
+
 
 def parse(to_parse):
     parser = ProverbsWikipageParser()
     parser.feed(to_parse)
     print len(parser.parsed)
     parser._parsed
+
 
 if __name__ == '__main__':
     fetch_wikpage()
