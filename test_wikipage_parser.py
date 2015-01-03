ac580339d9713d5314132566e695cc6a1ed4a602
Antoni Piotr Oleksicki <ap.oleksicki@gmail.com>
2015-01-03 14:31:43 +0100

    PEP 8 guidelines applied test_wikipage_parser.py

--- test_wikipage_parser.py
+++ test_wikipage_parser.py
@@ -12,14 +12,21 @@ def _simple_test(expected, to_parse):
 
 
 def test_simple_li():
-    _simple_test('Aliloka ĉielo estas sama ĉielo.', '<li>Aliloka ĉielo estas sama ĉielo.</li>')
+    _simple_test('Aliloka ĉielo estas sama ĉielo.',
+                 '<li>Aliloka ĉielo estas sama ĉielo.</li>')
+
 
 def test_li_with_href():
-    to_parse = '<li>Almozpetanto <a href="/wiki/Sin%C4%9Deno" title="Sinĝeno">sinĝena</a> restas kun sako malplena.</li>'
+    to_parse = '<li>Almozpetanto <a href="/wiki/Sin%C4%9Deno"'\
+    'title="Sinĝeno">sinĝena</a> restas kun sako malplena.</li>'
     _simple_test('Almozpetanto sinĝena restas kun sako malplena.', to_parse)
 
+
 def test_li_with_multiple_href():
-    to_parse = '<li>Vivi en <a href="/wiki/Silko" title="Silko">silko</a> kaj <a href="/wiki/Veluro" title="Veluro">veluro</a>, en <a href="/wiki/%C4%9Cojo" title="Ĝojo">ĝojo</a> kaj <a href="/wiki/Plezuro" title="Plezuro">plezuro</a>.</li>'
+    to_parse = '<li>Vivi en <a href="/wiki/Silko" title="Silko">silko</a> '\
+    'kaj <a href="/wiki/Veluro" title="Veluro">veluro</a>, en '\
+    '<a href="/wiki/%C4%9Cojo" title="Ĝojo">ĝojo</a> kaj '\
+    '<a href="/wiki/Plezuro" title="Plezuro">plezuro</a>.</li>'
     _simple_test('Vivi en silko kaj veluro, en ĝojo kaj plezuro.', to_parse)
 
 
