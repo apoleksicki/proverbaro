aa326a131042f51c89c0ad36bcef76764e1f65a1
Antoni Piotr Oleksicki <ap.oleksicki@gmail.com>
2015-01-20 23:56:23 +0100

    Added function for parsing a single definition of a word

--- test_translation_util.py
+++ test_translation_util.py
@@ -1,18 +1,31 @@
 # -*- coding: utf-8 -*-
 import json
-from translation_util import find_definition
+from translation_util import find_definition, _parse_single_definition
 
 def _read_json(filename):
     with open('test_resources\%s' % filename, 'r') as f:
         return json.loads(f.read())
 
 
-def _read_saluti_json(word):
-    return _read_json('saluti.json')
+def _read_saluti_json(filename):
+    def read_definition(word):
+        return _read_json(filename)
+    return read_definition
+
+
+def test_convert_saluti_definition_1():
+    expected = _read_json('converted_saluti_def_1.json')
+    saluti1def = _parse_single_definition(_read_json('saluti_def_1.json'))
+    assert expected == saluti1def
+
 
 def test_find_definition_of_saluti():
     expected = _read_json('converted_saluti.json')
-    salutidef = find_definition('saluti', _read_saluti_json)
-    print expected
-    print salutidef
+    salutidef = find_definition('saluti', _read_saluti_json('saluti.json'))
     assert expected == salutidef
+
+
+{'foo' : _read_json('saluti_def_1.json')['difino']}
+
+_parse_single_definition(_read_json('saluti_def_1.json'))
+
--- translation_util.py
+++ translation_util.py
@@ -2,7 +2,20 @@
 import urllib
 import json
 
-def find_definition(word, find_function):
+def _parse_single_definition(unparsedDefinition):
+    parsed = {'definition' : unparsedDefinition['difino']}
+    parsed['examples'] = [{'example' : example['ekzemplo']}
+                          for example in unparsedDefinition['ekzemploj']]
+    return parsed
+
+
+def _parse_definitions(unparsedDefinitions):
+#     return [_parse_single_definition(singleDefinition) for singleDefinition in unparsedDefinitions]
     pass
+
+
+def find_definition(word, find_function):
+    unparserDefinition = find_function(word)
+    return {'definitions' : _parse_definitions(unparserDefinition)}
 
 
