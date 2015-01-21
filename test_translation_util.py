c6c03ebae73e47df4b64e2cb710b7377d18e0b55
Antoni Piotr Oleksicki <ap.oleksicki@gmail.com>
2015-01-21 18:55:16 +0100

    Parsing of a definition fully implemented

--- test_translation_util.py
+++ test_translation_util.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import json
+
 from translation_util import find_definition, _parse_single_definition
 
 def _read_json(filename):
@@ -23,9 +24,3 @@ def test_find_definition_of_saluti():
     expected = _read_json('converted_saluti.json')
     salutidef = find_definition('saluti', _read_saluti_json('saluti.json'))
     assert expected == salutidef
-
-
-{'foo' : _read_json('saluti_def_1.json')['difino']}
-
-_parse_single_definition(_read_json('saluti_def_1.json'))
-
--- translation_util.py
+++ translation_util.py
@@ -3,19 +3,17 @@ import urllib
 import json
 
 def _parse_single_definition(unparsedDefinition):
-    parsed = {'definition' : unparsedDefinition['difino']}
-    parsed['examples'] = [{'example' : example['ekzemplo']}
-                          for example in unparsedDefinition['ekzemploj']]
+    parsed = {u'definition' : unparsedDefinition[u'difino']}
+    parsed[u'examples'] = [{u'example' : example[u'ekzemplo']}
+                          for example in unparsedDefinition[u'ekzemploj']]
     return parsed
 
 
 def _parse_definitions(unparsedDefinitions):
-#     return [_parse_single_definition(singleDefinition) for singleDefinition in unparsedDefinitions]
-    pass
+    return [_parse_single_definition(singleDefinition) for singleDefinition in unparsedDefinitions]
 
 
 def find_definition(word, find_function):
-    unparserDefinition = find_function(word)
-    return {'definitions' : _parse_definitions(unparserDefinition)}
-
-
+    unparsedDefinition = find_function(word)
+    return {u'definitions' : _parse_definitions(unparsedDefinition[u'difinoj']),
+            u'word' : unparsedDefinition['vorto']}
