2a29c4c518d791619944c9e0c355bab71b8b5807
Antoni Piotr Oleksicki <ap.oleksicki@gmail.com>
2015-06-14 20:41:13 +0200

    Function for removing grammatical endings
    Added a function, that removes accusative and plural endings from adjectives and nouns and converts verbs to infinitive forms.

--- translation_util.py
+++ translation_util.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
-import logging
 import urllib
 import json
-import pytest
+import string
 
 def _get_json(url):
     print url
@@ -24,7 +23,8 @@ def _parse_single_definition(unparsedDefinition):
 
 
 def _parse_definitions(unparsedDefinitions):
-    return [_parse_single_definition(singleDefinition) for singleDefinition in unparsedDefinitions]
+    return [_parse_single_definition(singleDefinition) for singleDefinition \
+        in unparsedDefinitions]
 
 
 def _find_full_definition(word, find_function):
@@ -45,13 +45,26 @@ def _lookup_word(word, find_function):
 
 def _remove_grammatical_endings(word):
     #Removes plural and accustive ending from the word
-    if word[-1] == 'j':
+
+    #Check nouns
+    if word.endswith('oj') or word.endswith('on'):
         return word[:-1]
-    if word[-1] == 'n':
-        if word[-2] == 'j':
-            return word[:-2]
-        else:
-            return word[:-1]
+    if word.endswith('ojn'):
+        return word[:-2]
+
+    #Check adjectives
+    if word.endswith('aj') or word.endswith('an'):
+        return word[:-1]
+    if word.endswith('ajn'):
+        return word[:-2]
+    
+    #Check verbs
+    if word.endswith('as') or word.endswith('is') or \
+        word.endswith('os') or word.endswith('us'):
+        return word[:-2] + 'i'
+    if word.endswith('u'):
+        return word[:-1]
+    return word
 
 def find_definition(word):
     lookupResult = _lookup_word(word, _lookup)
@@ -61,12 +74,39 @@ def find_definition(word):
         return None
 
 
+def split_proverb_into_words(proverb):
+    exclude = set(string.punctuation)
+    return ''.join(ch for ch in proverb if ch not in exclude).lower().split()
+
 class TestEndingStriping(object):
+    def test_ground_forms_not_changed(self):
+        assert 'patro' == _remove_grammatical_endings('patro')
+        assert 'grava' == _remove_grammatical_endings('grava')
+
     def test_plural_removal(self):
         assert 'patro' == _remove_grammatical_endings('patroj')
+        assert 'grava' == _remove_grammatical_endings('gravaj')
     
     def test_accusative_removal(self):
         assert 'patro' == _remove_grammatical_endings('patron')
+        assert 'grava' == _remove_grammatical_endings('gravan')
     
     def test_plural_and_accusative_removal(self):
-        assert 'patro' == _remove_grammatical_endings('patrojn')        
+        assert 'patro' == _remove_grammatical_endings('patrojn')
+        assert 'grava' == _remove_grammatical_endings('gravajn')
+
+    def test_plej_remains_unchanged(self):
+        assert 'plej' == _remove_grammatical_endings('plej')
+
+    def test_verbs_converted_to_inifinitive(self):
+        assert 'lerni' == _remove_grammatical_endings('lerni')
+        assert 'lerni' == _remove_grammatical_endings('lernas')
+        assert 'lerni' == _remove_grammatical_endings('lernis')
+        assert 'lerni' == _remove_grammatical_endings('lernos')
+        assert 'lerni' == _remove_grammatical_endings('lernus')
+        assert 'lerni' == _remove_grammatical_endings('lernis')
+
+class TestSentenceSplitting(object):
+    def test_split(self):
+        assert ['mizero', 'plej', 'ekstreme', 'dio', 'plej', 'proksime'] == \
+            split_proverb_into_words('Mizero plej ekstreme, Dio plej proksime.')
